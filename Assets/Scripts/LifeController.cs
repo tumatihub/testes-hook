d27a5b37523a7225ed4dec99149450b660909c0d
Yaggdryzzill <felfig@globo.com>
2018-08-28 22:27:33 -0300

    script de vida simples adicionado
    Script tem quantidade de vida e as funções de quanta vida recebe (OnHealthGain) e perde (OnDamage)

---
+++ Assets/Scripts/LifeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour {
+    public int health = 3;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnDamage(int damage)
+    {
+        health =- damage;
+    }
+
+    public void OnlifeGain(int life)
+    {
+        health = +life;
+    }
+}
